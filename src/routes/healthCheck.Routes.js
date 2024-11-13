ebf26cb2fcc959b9793df846447a3693a5872021
Manishpandey10 <manish2310pandey@gmail.com>
2024-11-13 15:42:29 +0530

    some fix

--- src/routes/healthCheck.Routes.js
+++ src/routes/healthCheck.Routes.js
@@ -7,7 +7,7 @@ checkRouter.route("/test").get(healthCheck);
 ///////////////////////////////////////////////////////////////////
 //i can use this below route as a middleware (kind of) in my app js, i f i donot wan't to use .get() request in the app.js so i specified the .get() in the route file itself so that when i have to use the route in app.js i can just use app.use() 
 
-//here checkRouter will do the run get() request healthCheck on the "/" route 
+//here checkRouter will do the run get request healthCheck on the "/" route 
 checkRouter.get("/",healthCheck);
 /************************************************************ */
 
